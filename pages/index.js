3a7a0e7b40572af88613a9890e85782ae5a9f79d
Ashish Godiyal <ashishgodiyal.333@gmail.com>
2023-04-19 16:44:38 +0530

    updated

--- pages/index.js
+++ pages/index.js
@@ -79,7 +79,7 @@ export default function Home() {
             pages.map(page=> <li key={page}>
             <p
               
-              className={`cursor-pointer px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white` + ((curPage==page)?' font-bold text-black bg-gray-400':'')}
+              className={`cursor-pointer px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white` + ((curPage==page)?' font-bold text-black bg-gray-300':'')}
               onClick={()=> setCurPage(page)}
             >
               {page}
